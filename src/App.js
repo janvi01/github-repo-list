db9e2f195f9f02e2991701b686f6d70f83491007
Janvi Bajoria <janvibajo1@gmail.com>
2023-07-17 16:57:57 +0530

    added github icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Heading, Box } from "@chakra-ui/react";
+import { Heading, Box, HStack, Icon } from "@chakra-ui/react";
 import React from "react";
 import User from "./components/User";
+import { BsGithub } from "react-icons/bs";
 
 function App() {
   return (
     <Box textAlign={"center"} p="8" bgColor={"black"} minHeight={"100vh"}>
-      <Heading>Github Repo List</Heading>
+      <HStack justify={"center"} spacing={"6"}>
+        <Icon as={BsGithub} boxSize={12} />
+        <Heading>Github Repo List</Heading>
+      </HStack>
       <User />
     </Box>
   );
